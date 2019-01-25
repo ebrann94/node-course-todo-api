a20b44e0149d3ad09b8e0cb184c72087450648d8
Ethan Brann <ethan.brann94@gmail.com>
2019-01-25 13:27:50 +0000

    test POST /todos route

--- server/server.js
+++ server/server.js
@@ -6,10 +6,24 @@ const {Todo} = require('./models/Todo');
 const {User} = require('./models/User');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
 
+    todo.save().then(doc => {
+        res.send(doc);
+    }, err => {
+        res.send(err);
+    })
 });
+
 app.listen(3000, () => {
-    Console.log('Server running on port 300');
-})
+    console.log('Server running on port 3000');
+});
+
+module.exports = {
+    app
+};
